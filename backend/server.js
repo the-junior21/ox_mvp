01afea340e6c17ff07b1ba9c06854367c323733a
the-junior21 <160070887+the-junior21@users.noreply.github.com>
2026-01-30 11:40:10 +0100

    Add server port configuration and listener

--- backend/server.js
+++ backend/server.js
@@ -34,4 +34,8 @@ mongoose
   .then(() => console.log("MongoDB connected ✅"))
   .catch(err => console.error(err));
 
+ const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
 export default app
