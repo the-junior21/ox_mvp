f054d5cb32f70440a0029435a3dcad76a7793d1d
junior <hamdidajr@email.com>
2026-02-04 23:16:19 +0100

    not required

--- backend/models/User.js
+++ backend/models/User.js
@@ -24,7 +24,7 @@ const UserSchema = new mongoose.Schema(
       },
       coordinates: {
         type: [Number], // [lng, lat]
-        required: true,
+        required: false,
       },
     },
   },
