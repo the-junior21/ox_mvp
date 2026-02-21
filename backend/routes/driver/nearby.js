4be441640b7952623407719dacab229b3ad3f460
junior <hamdidajr@email.com>
2026-02-21 18:50:23 +0100

    edit nearvy chack

--- backend/routes/driver/nearby.js
+++ backend/routes/driver/nearby.js
@@ -9,8 +9,7 @@ function getDistanceKm(lat1,lng1,lat2,lng2){
     const R = 6371
     const dLat = (lat2 - lat1) * Math.PI / 180
     const dLng = (lng2 - lng1) * Math.PI / 180
-    const a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180)
-    * Math.sin(dLng/2) * Math.sin(dLng/2)
+    const a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) * Math.sin(dLng/2) * Math.sin(dLng/2)
     const c = 2 * Math.atan2(Math.sqrt(a),Math.sqrt(1-a))
     return R * c
 
@@ -33,6 +32,7 @@ router.post("/", async (req, res) => {
             "location.coordinates.1":{$exists:true}
         })
         const nearbyDrivers = drivers.filter(driver =>{
+            if(!driver.location || !driver.location.coordinates) return false
             const [driverLng,driverLat] = driver.location.coordinates
             const distance = getDistanceKm(
                 lat,
@@ -61,7 +61,7 @@ router.post("/", async (req, res) => {
     } catch (err) {
         res.status(500).json({ 
             message: "Check if you have a 2dsphere index!", 
-            error: err.message 
+            error: err.message
         })
     }
 })
