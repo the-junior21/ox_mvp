da36628482847891b9d75a9c3755a7457630105a
junior <hamdidajr@email.com>
2026-03-04 22:21:17 +0100

    i modified ride req route

--- backend/routes/rideRequest.js
+++ backend/routes/rideRequest.js
@@ -4,6 +4,17 @@ import {io,onlineDrivers} from "../server.js"
 import User from "../models/User.js"
 
 const router = express.Router()
+
+function getDistanceKm(lat1,lng1,lat2,lng2){
+    const R = 6371
+    const dLat = (lat2 - lat1) * Math.PI / 180
+    const dLng = (lng2 - lng1) * Math.PI / 180
+    const a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) * Math.sin(dLng/2) * Math.sin(dLng/2)
+    const c = 2 * Math.atan2(Math.sqrt(a),Math.sqrt(1-a))
+    return R * c
+
+}
+
 router.post("/",async(req,res)=>{
     try{
         const {passengerId ,depart,destination,pickupLocation} = req.body
@@ -25,7 +36,7 @@ router.post("/",async(req,res)=>{
                 name:depart,
                 location:{
                     type:"Point",
-                    coordinates:[pickupLocation.lng,pickupLocation.lat],
+                    coordinates:[/*pickupLocation.*/lng,/*pickupLocation.*/lat],
                 },
             },
             destination:{
@@ -41,32 +52,45 @@ router.post("/",async(req,res)=>{
         const drivers = await User.find({
                     role:"driver",
                     isOnline:true,
-                    location: {
-                        $near: {
-                            $geometry: {
-                                type: "Point",
-                                coordinates: [parseFloat(lng), parseFloat(lat)] // [Longitude, Latitude]
-                            },
-                            $maxDistance: 100000 // 100 kilometers
-                        }
-                    }
+                    "location.lat":{$exists:true},
+                    "location.lng":{$exists:true}
+                    
                 })    
                 console.log("Drivers found:", drivers.length);
 
                 console.log("Online drivers map:", onlineDrivers);
+                const nearbyDrivers = drivers.filter(driver =>{
+            if(!driver.location) return false
+            const driverLat = driver.location.lat
+            const driverLng = driver.location.lng
+            const distance = getDistanceKm(
+                lat,
+                lng,
+                driverLat,
+                driverLng
+            )
+            return distance <= 100
+        })
+        console.log("nearby drivers : ",nearbyDrivers.length)
 
-                drivers.forEach(driver =>{
+
+
+                nearbyDrivers.forEach(driver =>{
                             const socketId = onlineDrivers.get(driver._id.toString())
                             if(socketId){
                                 io.to(socketId).emit("new_ride_request",{
                                     rideId :ride._id,
-                                    pickupLocation: { name: depart, location: { lat, lng } },
+                                    pickupLocation: {lat, lng,name: depart } ,
                                     destination: { name: destination }
                                 })
                                   console.log("Driver:", driver._id.toString(), "Socket:", socketId);
                             }
                         
 })
+res.json({
+            count: nearbyDrivers.length,
+            results: nearbyDrivers
+        })
     }catch(error){
         console.error("Ride Request Error: ",error)
         res.status(500).json({
